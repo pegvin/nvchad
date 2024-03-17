62eb33994d89c2b385ad8cc99276f0533e0418c8
Aditya <75035219+pegvin@users.noreply.github.com>
2024-03-17 20:31:58 +0530

    Initial commit

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "c",
+    "cpp",
+    "make",
+    "cmake",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- web dev stuff
+    "html-lsp",
+
+    -- c/cpp stuff
+    "clangd",
+
+    -- shell stuff
+    "shfmt",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,12 @@
+local options = {
+  ensure_installed = { "lua", "vim", "c", "cmake", "css", "make", "html" },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
